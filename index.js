a16b462b76302b1b05558b07e55be9086b7340f3
Prasanna Balaji <prasanna9417@gmail.com>
2019-10-05 11:51:38 +0530

    first commit'

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const port = 3010
+
+const server = http.createServer(function(request, response){
+    if(request.url == '/'){
+        response.setHeader('Content-Type', 'application/json')
+        response.end(JSON.stringify('Hello World'))
+    }
+})
+
+server.listen(port, function(){
+    console.log('listening on port', port)
+})
